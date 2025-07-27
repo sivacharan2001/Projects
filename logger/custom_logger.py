0198ae3a724303540a8465c1ca0f52d949f9f533
sivacharan2001 <sivacharany2001@gmail.com>
2025-07-27 12:32:29 +0530

    custom exception code is update

--- logger/custom_logger.py
+++ logger/custom_logger.py
@@ -9,14 +9,13 @@ class CustomLogger:
        os.makedirs(logs_dir, exist_ok=True)
 
        log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-       log_file_path = os.path.join(logs_dir, log_file)
+       log_file_path = os.path.join(self.logs_dir, log_file)
 
        logging.basicConfig(
             filename=log_file_path,
             format="[%(asctime)s] - %(levelname)s %(name)s (line: %(lineno)d) - %(message)s",
             level=logging.INFO
         )
-
 
     def get_logger(self, name=__file__):
         return logging.getLogger(os.path.basename(name))
